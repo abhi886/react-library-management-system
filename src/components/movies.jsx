557e4f363d3ea91235593e5b8a72bb8a0f9d5389
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-06-10 12:02:19 +1000

    11. Filtering Default Props

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -54,8 +54,6 @@ class Movies extends Component {
             <div className='col-2'>
               <ListGroup
                 items={genres}
-                textProperty='name'
-                valueProperty='_id'
                 onItemSelect={this.handleGenreSelect}
               ></ListGroup>
             </div>
