a37c9370b8ad322262a609409997e6e8caea1caa
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-06-11 12:09:40 +1000

    15. Sorting - Extracting MoviesTbale

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
+import MoviesTable from "./moviesTable";
 import { getMovies } from "../services/fakeMovieService";
-import Like from "./common/like";
 import Pagination from "./common/pagination";
 import { paginate } from "../utils/paginate";
 import ListGroup from "./common/listGroup";
 import { getGenres } from "../services/fakeGenreService";
-
+// import MoviesTable from "./MoviesTable";
 class Movies extends Component {
   state = {
     movies: [],
@@ -71,44 +71,11 @@ class Movies extends Component {
             </div>
             <div className='col-8'>
               <p>Showing {filtered.length} movies in the database</p>
-              <table className='table'>
-                <thead>
-                  <tr>
-                    <th>Title</th>
-                    <th>Genre</th>
-                    <th>Stock</th>
-                    <th>Rate</th>
-                    <th></th>
-
-                    <th></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {movies.map((movie) => (
-                    <tr key={movie._id}>
-                      <td>{movie.title}</td>
-                      <td> {movie.genre.name}</td>
-                      <td>{movie.numberInStock}</td>
-                      <td> {movie.dailyRentalRate}</td>
-                      <td>
-                        <Like
-                          liked={movie.liked}
-                          onClick={() => this.handleLike(movie)}
-                        />
-                      </td>
-                      <td>
-                        <button
-                          onClick={() => this.handleDelete(movie)}
-                          className='btn btn-danger btn-sm'
-                        >
-                          Delete
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-
+              <MoviesTable
+                movies={movies}
+                onLine={this.handleLike}
+                onDelete={this.handleDelete}
+              />
               <Pagination
                 itemsCount={filtered.length}
                 currentPage={currentPage}
