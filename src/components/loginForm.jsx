e6cd0e81bdffbaf8ffe7f087d63fd70807d0b98c
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-07-03 11:31:48 +1000

    Validation on Change

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -20,18 +20,32 @@ class LoginForm extends Component {
     return Object.keys(errors).length === 0 ? null : errors;
   };
 
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username is required.";
+    }
+
+    if (name === "password") {
+      if (value.trim() === "") return "Password is required.";
+    }
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
     const errors = this.validate();
-    // console.log(errors);
     this.setState({ errors: errors || {} });
     if (errors) return;
   };
 
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
   render() {
     const { account, errors } = this.state;
