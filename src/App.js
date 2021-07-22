d51e4e733414f1893c772b09f582e296d4ebcb23
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-07-22 14:03:41 +1000

    Protect Routes 19

--- src/App.js
+++ src/App.js
@@ -80,6 +80,7 @@ class App extends Component {
   };
 
   render() {
+    const { user } = this.state;
     console.log("App-Renderd");
     return (
       <>
@@ -103,7 +104,13 @@ class App extends Component {
           <Route path='/register' component={RegisterForm} />
           <Route path='/login' component={LoginForm} />
           <Route path='/logout' component={Logout} />
-          <Route path='/movies/:id' component={MovieForm} />
+          <Route
+            path='/movies/:id'
+            render={(props) => {
+              if (!user) return <Redirect to='/login' />;
+              return <MovieForm {...props} />;
+            }}
+          />
           <Route
             path='/movies'
             render={(props) => <Movies {...props} user={this.state.user} />}
