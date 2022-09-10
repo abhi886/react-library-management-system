e03690a41396903b94992e78668fc8c45add1fc9
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-09-10 11:35:07 +1000

    Responsive Landing Page description paragraph on small screen

--- src/components/landingPage.jsx
+++ src/components/landingPage.jsx
@@ -4,10 +4,12 @@ import RegisterForm from "./registerForm";
 function landingPage() {
   return (
     <div className='container'>
-      <div class='row align-items-center g-lg-5 py-5'>
-        <div class='col-lg-7 text-center text-lg-start'>
-          <h1 class='display-4 fw-bold lh-1 mb-3'>Library Management System</h1>
-          <p class='col-lg-10 fs-4'>
+      <div className='row align-items-center g-lg-5 py-5'>
+        <div className='col-lg-7 text-center text-lg-start'>
+          <h1 className='display-4 fw-bold lh-1 mb-3'>
+            Library Management System
+          </h1>
+          <p className='col-lg-10 fs-4 indexTitleDescription'>
             It’s the easiest, fastest way to manage your books. Created using
             the MERN stack, Library Management System is an open source project
             hosted over github. Feel free to contribute.
